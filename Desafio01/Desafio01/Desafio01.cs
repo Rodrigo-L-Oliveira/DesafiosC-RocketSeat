4bb6ab80dea28715bbd79c7ffca9c05cdac4568e
Rodrigo-L-Oliveira <contato.rodrigo.oliveiraa@gmail.com>
2024-10-28 07:21:55 -0300

    Primeiros desafios

---
+++ Desafio01/Desafio01/Desafio01.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada.
+ Com o nome dele é exibida:  "Olá, Welisson! Seja muito bem-vindo!"
+ */
+
+using System;
+
+internal class Desafio01
+{
+    static void Main()
+    {
+        Console.Write("Por favor, digite seu nome: ");
+        string nome = Console.ReadLine(); // Recebe o nome do usuário.
+
+        Console.WriteLine("Digite seu gênero (M para Masculino, F para Feminino): ");
+        string genero = Console.ReadLine(); // Recebe o gênero do usuário.
+
+        string saudacao = genero.ToUpper() == "M" ? "Bem-vindo" : genero.ToUpper() == "F" ? "Bem-vinda" : "Bem-vindo(a)";
+        // Valida o gênero do usuário para poder fazer a saudação correta
+        // (caso o usuário não responda aparece: "Bem-vindo(a)".
+
+        Console.WriteLine($"Olá, {nome}! Seja muito {saudacao}");
+        // Mostra na tela a saudação com nome do usuário e a devida saudação.
+    }
+}
---
+++ Desafio02/Desafio02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exiba o nome completo.
+ */
+
+using System;
+
+namespace Desafios
+{
+    internal class Desafio02
+    {
+        static void Main()
+        {
+            // Strings que recebem o nome e sobrenome do usuário.
+
+            Console.Write("Informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Bem vindo: {nome} {sobrenome}");
+        }
+    }
+}
